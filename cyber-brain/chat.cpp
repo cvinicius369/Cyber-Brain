a9d9d2fe93dffe66be049c72404b815f403353e0
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-03-01 15:28:03 +0000

    Chances

---
+++ cyber-brain/chat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>     // biblioteca excenssial para o bom funcionamento do software                           
+#include <cmath>        // biblioteca que ajuda a realizar calculos matematicos complexos
+#include <vector>       // biblioteca que ajuda a manipular vetores e matrizes
+#include <string>       // bibliotexa que permite o uso de strings
+#include <ctime>        // biblioteca que permite a manipulacao do tempo
+#include "nucleo_a.h"
+using namespace std;    // espaco que permite o uso de funcoes sem mencionar o termo "std::nome_da_funcao"
+
+int main(){
+    menu();
+    return 0;   
+}
---
+++ cyber-brain/nucleo_a.h
@@ -0,0 +1,162 @@
+#ifndef conection_h
+#define conection_h
+#include <iostream>     // biblioteca excenssial para o bom funcionamento do software                           
+#include <cmath>        // biblioteca que ajuda a realizar calculos matematicos complexos
+#include <vector>       // biblioteca que ajuda a manipular vetores e matrizes
+#include <string>       // bibliotexa que permite o uso de strings
+#include <ctime>        // biblioteca que permite a manipulacao do tempo
+#include <iomanip>      //
+#include "nucleo_a.h"
+using namespace std;    // espaco que permite o uso de funcoes sem mencionar o termo "std::nome_da_funcao"
+
+class Styles{
+    public:
+        void LineStyle(){
+            cout << "---------------------------------------------------------------------------" << endl;
+        }
+        void BannerNeural(){
+            cout << "---------------------------------------------------------------------------" << endl;
+            cout << "///////////////////////////// SISTEMA NEURAL /////////////////////////////" << endl;
+            cout << "---------------------------------------------------------------------------" << endl;
+        }
+};
+
+class Neuronio {
+private:
+    double limiar, bias;     // o limiar tem o papel do peso que define o quao importante um neuronio eh
+public:
+    Neuronio(double axonio, double bias) : limiar(axonio), bias(bias) {} // construtor da classe
+    double feedforward(double axonio) { return axonio * axonio + bias; } // funcao d ativacao
+
+    double alteralimiar(double dado)  { this->limiar = limiar + dado; return this->limiar; }
+
+    void treinar(double entrada, double alvo) {     // funcao que treina o neuronio
+        double saida = feedforward(entrada);        // ativa o neuronio e armazena a saida dele
+        double erro = alvo - saida;                 // Calcula o erro
+
+        // Ajusta o limiar e o bias usando o gradiente descendente
+        limiar += 0.1 * erro * entrada;             // os digitos em double sao as taxas de aprendizado
+        bias += 0.1 * erro;
+
+        cout << "Peso -> " << limiar << "\nBias -> " << bias << endl;   // mostrando o peso e o bias
+        cout << "Resultado: " << entrada * limiar + bias << endl;       // mostrando a saida
+    }
+
+};
+class RedeNeural {
+    Neuronio neuronio1, neuronio2, neuronio_saida;
+
+public:
+    RedeNeural(double w1, double b1, double w2, double b2, double ws, double bs) : neuronio1(w1, b1), neuronio2(w2, b2), neuronio_saida(ws, bs) {}
+
+    double feedforward(double x) {
+        double saida_neuronio1 = neuronio1.feedforward(x);
+        double saida_neuronio2 = neuronio2.feedforward(x);
+
+        double soma = saida_neuronio1 + saida_neuronio2;
+        double saida_rede = neuronio_saida.feedforward(soma);
+
+        return saida_rede;
+    }
+    void treinar(vector<double> entradas, vector<double> alvos) {
+        for (int i = 0; i < entradas.size(); i++) {
+            double entrada = entradas[i];
+            double alvo = alvos[i];
+
+            // Treina cada neurônio na rede
+            neuronio1.treinar(entrada, alvo);
+            neuronio2.treinar(entrada, alvo);
+
+            neuronio_saida.treinar(neuronio1.feedforward(entrada) + neuronio2.feedforward(entrada), alvo);
+        }
+    }
+};
+
+void ValidarRedeNeural(double x, double y){
+    double n1, n2, ns, s1, s2, ss;
+    RedeNeural sinapse2(n1, n2, ns, s1, s2, ss);
+    n1 = 0; n2 = 0; ns = 0; 
+    s1 = 0; s2 = 0; ss = 0;
+
+
+    int ny = round(y); int nx = round(x);
+    int z = ny % nx;
+    int i =  0;
+
+    if ( (z == 0) ){
+        cout << "Saída funcionando corretamente!" << endl;
+    } else {
+        while ( (z != 0) )
+        {
+
+            RedeNeural sinapse2(n1, n2, ns, s1, s2, ss);
+            cout << "Resultado em teste: " << sinapse2.feedforward(x) << endl;
+            double y2 = sinapse2.feedforward(x);
+
+            int ny2 = round(y2);
+            z = ny2 % nx;
+
+            n1 = n1 + 0.0001; n2 = n2 + 0.0001; ns = ns + 0.0001; 
+            s1 = s1 + 0.0001; s2 = s2 + 0.0001; ss = ss + 0.0001;
+
+        }
+        cout << "Resultado corrigido: " << sinapse2.feedforward(x) << endl;
+    }
+}
+
+void LimiarDeAtivacao(double axonio) {
+    Styles s;
+    if ((axonio > 0.0) && (axonio < 2.0)) {
+        double w1, b1, w2, b2, ns, bs, x;
+        int decisao;
+
+        cout << "Informe o valor de entrada para Neuronio-1: "; cin >> w1;      // definindo peso do neuronio 1
+        cout << "Agora o valor alvo para Neuronio-1: ";         cin >> b1;      // definindo bias do neuronio 1
+        cout << "Agora valor de entrada do Neuronio-2: ";       cin >> w2;      // peso do neuronio 2
+        cout << "Agora o valor alvo do Neuronio-2: ";           cin >> b2;      // bias do neuornio 2
+        cout << "Valor de entrada do Neuronio de Saida: ";      cin >> ns;      // peso do neuronio de saida
+        cout << "Valor alvo do Neuronio de Saida: ";            cin >> bs;      // bias do neuronio de saida
+        cout << "Agora informe qual o valor de x: ";            cin >> x;       // valor de x
+
+        RedeNeural sinapse1(w1, b1, w2, b2, ns, bs);
+
+        vector<double> entradas = { 1, 2, 3, 4, 5 };
+        vector<double> alvos = { 1, 4, 9, 16, 25 };
+
+        for (int i = 0; i <= 3; i++) {  // funcao for para que o treinamento seja repetido até que tenha os valores ideais
+            cout << "Alvos: " << alvos[0] << ", " << alvos[1] << ", " << alvos[2] << ", " << alvos[3] << ", " << alvos[4];
+            sinapse1.treinar(entradas, alvos);
+        }
+        s.LineStyle();
+        sinapse1.feedforward(x);
+        cout << "A saida da rede neural para x=" << x << " e: " << sinapse1.feedforward(x) << endl;
+        s.LineStyle();
+        ValidarRedeNeural(x, sinapse1.feedforward(x));
+        s.LineStyle();
+    }
+    else {
+        cout << "Neuronio em Repouso!" << endl << "Recuperando Limiar" << endl;
+        axonio -= 0.1;
+        LimiarDeAtivacao(axonio);
+    }
+}
+
+int menu() {
+    Styles s;
+    int action;
+
+    cout << "[1] - Sistema Neural \n->"; cin >> action;
+    s.BannerNeural();
+    if (action == 1) {
+        double axonio;
+
+        cout << "Informe o valor do estimulo: "; cin >> axonio;
+        LimiarDeAtivacao(axonio);
+    }
+
+    system("pause");
+    return 0;
+}
+
+
+#endif
